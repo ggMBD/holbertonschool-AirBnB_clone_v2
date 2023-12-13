12d6e33700aa8e8796c8bbb625035275ca3a6782
maouia <maouiabend@gmail.com>
2023-12-13 23:11:49 +0100

    pep8

--- models/amenity.py
+++ models/amenity.py
@@ -18,8 +18,8 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship(
-        'Place',
-        secondary='place_amenity',
-       viewonly=False,
-       back_populates="amenities"
+        "Place",
+        secondary="place_amenity",
+        viewonly=False,
+        back_populates="amenities"
     )
